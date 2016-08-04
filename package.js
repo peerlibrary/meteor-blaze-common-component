33d99d8d945fda6bd336fd0684a463817e157bbd
Mitar <mitar.git@tnode.com>
2016-08-03 21:04:41 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:blaze-common-component',
   summary: "An extended base Blaze Component with common features",
-  version: '0.3.0',
+  version: '0.4.0',
   git: 'https://github.com/peerlibrary/meteor-blaze-common-component.git'
 });
 
