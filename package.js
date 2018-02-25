bc40918ca10f596ca69c5444e85a472cbcb9ddcd
Mitar <mitar.git@tnode.com>
2018-02-25 02:35:54 -0800

    Updating dependencies.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:blaze-common-component',
   summary: "An extended base Blaze Component with common features",
-  version: '0.4.1',
+  version: '0.4.2',
   git: 'https://github.com/peerlibrary/meteor-blaze-common-component.git'
 });
 
@@ -18,16 +18,16 @@ Package.onUse(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:blaze-components@0.20.0',
-    'momentjs:moment@2.15.2',
+    'peerlibrary:blaze-components@0.22.0',
+    'momentjs:moment@2.20.1',
     'peerlibrary:assert@0.2.5'
   ]);
 
   // Optional dependencies.
   api.use([
-    'peerlibrary:flow-router@2.10.1_1',
-    'kadira:flow-router@2.10.1',
-    'peerlibrary:user-extra@0.1.0'
+    'peerlibrary:flow-router@2.12.1_1',
+    'kadira:flow-router@2.12.1',
+    'peerlibrary:user-extra@0.4.0'
   ], {weak: true});
 
   api.export('CommonComponent');
@@ -51,7 +51,7 @@ Package.onTest(function (api) {
   // Internal dependencies.
   api.use([
     'peerlibrary:blaze-common-component',
-    'peerlibrary:blaze-components@0.20.0'
+    'peerlibrary:blaze-components@0.22.0'
   ]);
 
   // 3rd party dependencies.
