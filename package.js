754be20e6b309796be3582f79ecfff652db8b73f
Mitar <mitar.git@tnode.com>
2019-09-22 23:03:27 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,33 +1,33 @@
 Package.describe({
   name: 'peerlibrary:blaze-common-component',
   summary: "An extended base Blaze Component with common features",
-  version: '0.4.3',
+  version: '0.5.0',
   git: 'https://github.com/peerlibrary/meteor-blaze-common-component.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.4.0.1');
+  api.versionsFrom('1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript',
+    'coffeescript@2.4.1',
     'underscore',
-    'spacebars',
+    'spacebars@1.0.15',
     'tracker'
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:blaze-components@0.22.0',
-    'momentjs:moment@2.20.1',
-    'peerlibrary:assert@0.2.5'
+    'peerlibrary:blaze-components@0.23.0',
+    'momentjs:moment@2.24.0',
+    'peerlibrary:assert@0.3.0'
   ]);
 
   // Optional dependencies.
   api.use([
     'peerlibrary:flow-router@2.12.1_1',
     'kadira:flow-router@2.12.1',
-    'peerlibrary:user-extra@0.3.0'
+    'peerlibrary:user-extra@0.5.0'
   ], {weak: true});
 
   api.export('CommonComponent');
@@ -41,24 +41,24 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('1.4.0.1');
+  api.versionsFrom('1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript',
-    'jquery',
+    'coffeescript@2.4.1',
+    'jquery@1.11.11',
     'tracker'
   ]);
 
   // Internal dependencies.
   api.use([
     'peerlibrary:blaze-common-component',
-    'peerlibrary:blaze-components@0.22.0'
+    'peerlibrary:blaze-components@0.23.0'
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.2.26'
+    'peerlibrary:classy-test@0.4.0'
   ]);
 
   api.addFiles([
